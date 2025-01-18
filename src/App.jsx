2e137d0d79b6bb7d49f97690489c7bd782eeea8a
rohitjainfsl <rohit@fullstacklearning.com>
2025-01-18 12:41:08 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="w-96 mx-auto py-4 text-center">
+        <h1 className="mb-3 text-5xl font-bold">POKE WORLD</h1>
+        <div className="flex items-center justify-between gap-4">
+          <select name="" id="" className="w-1/2 border-2"></select>
+          <input type="text" className="w-1/2 border-2" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
