8bc1d07f1933f75d50cc818366b02372ef5b69a2
rohitjainfsl <rohit@fullstacklearning.com>
2025-01-19 21:18:08 +0530

    filtering by type

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,15 @@ function App() {
       setLoading(false);
     }
   }
+
+  function handleTypeChange(e) {
+    const copy = pokemonsToDisplay;
+    if (e.target.value === "all") {
+      setPokemonsToDisplay(copy);
+    } else {
+      
+    }
+  }
   console.log(pokemonsToDisplay);
   return (
     <>
@@ -62,13 +71,18 @@ function App() {
           <select
             name=""
             id=""
-            className="w-1/3 bg-white border border-slate-300 rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            className="w-1/3 bg-white border border-slate-300 rounded-md py-2 px-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm capitalize"
+            defaultValue=""
+            onChange={handleTypeChange}
           >
-            <option value="" selected disabled>
+            <option value="" disabled>
               Filter By Type
             </option>
+            <option value="all">All Types</option>
             {allTypes.map((typeObject, index) => (
-              <option key={index}>{typeObject.name}</option>
+              <option key={index} value={typeObject.name}>
+                {typeObject.name}
+              </option>
             ))}
           </select>
           <label className="w-2/3 relative block">
